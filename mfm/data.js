7a5a81a23f56ba3ca689cd21d3e9330ce9127c35
chldbwls <chldbwls8643@naver.com>
2023-06-12 01:57:21 +0900

    mbti effect done ver

---
+++ mfm/data.js
@@ -0,0 +1,130 @@
+const qnaList=[
+    {
+        q: 'Q1. I feel at ease at times like this',
+        a: [
+            {answer: 'a. When you hang out with people', type:['ESFP','ESFJ','ESTP','ESTJ','ENFP','ENFJ','ENTP','ENTJ']},
+            {answer: 'b. When I rest at home alone', type:['ISFP','ISFJ','ISTP','ISTJ','INFJ','INFP','INTJ','INTP']}
+        ]
+    },
+    {
+        q: 'Q2. when a problem arises',
+        a: [
+            {answer: 'a. I accept the reality', type:['ESFP','ESFJ','ESTP','ESTJ','ISFP','ISFJ','ISTP','ISTJ']},
+            {answer: 'b. I wonder why this happened to me', type:['ENFP','ENFJ','ENTJ','ENTP','INFP','INFJ','INTP','INTJ']}
+        ]
+    },
+    {
+        q: 'Q3. If your friend told you that he was depressed and bought a flowerpot',
+        a: [
+            {answer: 'a. Why were you depressed?', type:['ENFP','ENFJ','ESFP','ESFJ','INFP','INFJ','ISFJ','ISFP']},
+            {answer: 'b. What plant did you buy?', type:['ENTP','ENTJ','ESTP','ESTJ','ISFP','ISFJ','INFP','INFJ']}
+        ]
+    },
+    {
+        q: 'Q4. Before I do something',
+        a: [
+            {answer: 'a. to plan and implement a plan', type:['ENFJ','ENTJ','ESTJ','ESFJ','INTJ','ISTJ','ISFJ','INFJ']},
+            {answer: 'b. Do it first without a plan', type:['ENFP','ENTP','ESTP','ESFP','ISFP','ISTP','INFP','INTP']}
+        ]
+    }
+]
+
+const infoList=[
+    {
+        name: 'INFJ',
+        sub: 'The beginning of the morning',
+        desc: 'An introvert but idealistic person. A type of person whose beliefs matter. I recommend mysterious meditation music that suits your time alone.',
+        rating: '3.8%'
+    },
+    {
+        name:'INTJ',
+        sub:'Acceptance meditation on self-loathing, shame',
+        desc:'A perfectionist who wants to excel at everything. However, since a person cannot be perfect, sometimes it is necessary to have the courage to put it down.',
+        rating: '5.5%'
+    },
+    {
+        name:'ISTJ',
+        sub:'1 minute meditation',
+        desc:'A person who is always sincere and responsible, but sometimes stressed by excessive responsibility. A short meditation suits this personality, where practicality is the best.',
+        rating: '14.7%'
+    },
+    {
+        name:'ISFJ',
+        sub:'Anytime Anywhere Short Loyal Meditation Immediately',
+        desc:'A type of devoted personality that is full of consideration for others. However, one often does not consider ones feelings. Lets acknowledge your feelings and encourage them.',
+        rating: '8.4%'
+    },
+    {
+        name:'ISTP',
+        sub:'cloud meditation while looking at the sky',
+        desc:'You like to focus on something by yourself, but you also get bored easily! Why do not you meditate while looking at the clouds in the sky that show a different side of you every day',
+        rating: '4.7%'
+    },
+    {
+        name:'ISFP',
+        sub:'Choose lunch menu.',
+        desc:'You are gentle, humble, and know how to enjoy the ease of life. But you are sometimes told that you find it difficult to say no to others and are indecisive. I recommend you to practice meditation by choosing a lunch menu.',
+        rating: '5.9%'
+    },
+    {
+        name:'ENTJ',
+        sub:'Short meditation on bed as soon as you wake up',
+        desc:'An able man who always handles things with confidence with bold leadership. You who are already planning what to do all the time. Lets start a lively day with meditation!',
+        rating: '3.8%'
+    },
+    {
+        name:'ESTJ',
+        sub:'Relaxed Body Scan Before Sleep',
+        desc:'You are realistic and seeking perfection. I recommend a body scan that brings stability to the body and mind together',
+        rating: '10.7%'
+    },
+    {
+        name:'ESFJ',
+        sub:'Name the emotion that comes to mind now Meditation',
+        desc:'You are affectionate and sociable who takes good care of people around you! Sometimes you will be happier if you admit that other peoples feelings may be different from mine and practice looking into my heart',
+        rating: '6.6%'
+    },
+    {
+        name:'ENTP',
+        sub:'Yoon Dong-joo - Seoshi',
+        desc:'For those of you who always enjoy new challenges with rich imagination! I recommend a different way of meditation. Meditation in writing. It is Poetry Meditation',
+        rating: '3.5%'
+    },
+    {
+        name:'ENFP',
+        sub:'Morning meditation starting with a poem by Rumi',
+        desc:'Emotional and active, you like to hang out with people freely. Sometimes, when you have a hard time with mood swings, try meditating to greet your emotions in a kind way.',
+        rating: '8.4%'
+    },
+    {
+        name:'ESTP',
+        sub:'Watch your mind in a noisy place',
+        desc:'You are outgoing and adventurous. For those who enjoy outdoor activities, I recommend meditation that can be done in crowded or noisy places',
+        rating: '4.4%'
+    },
+    {
+        name:'ESFP',
+        sub:'3 step breathing meditation to access joy',
+        desc:'Pleasure is important to you, a free soul. Sometimes on tiring and hard days, I recommend meditation to find pleasure cells again',
+        rating: '5.3%'
+    },
+    {
+        name:'INTP',
+        sub:'[Book Introduction] Acknowledge Yourself Meditation Fearless Mind',
+        desc:'You love logical and intelligent exploration, but you are prone to depression due to criticism and evaluation of yourself. Lets practice accepting me for who I am',
+        rating: '4.3%'
+    },
+    {
+        name:'INFP',
+        sub:'Charity Meditation Award',
+        desc:'You are generous to others, but you can not say hateful things, so you are going to die inside. If your acceptance area reaches its limit, you can fall into deep depression. Practice being generous to yourself',
+        rating: '6.5%'
+    },
+    {
+        name:'ENFJ',
+        sub:'Meditation that wishes me happiness anytime, anywhere',
+        desc:'For you, who are social, sympathetic, and creative, it is good to wish your happiness. It is good to think about the kind actions or gifts you have received from yourself or others, and to think of your happiness through appreciation meditation',
+        rating: '3.5%'
+    }
+
+]
